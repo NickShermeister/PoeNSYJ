1c29c48179e45eb1c9c186a55568e171f3d34282
Nicholas Sherman <nsherman@olin.edu>
2017-09-05 10:30:32 -0400

    Final code push, including interrupts and debouncing.

--- potentiometer_inclusive/potentiometer_inclusive.ino
+++ potentiometer_inclusive/potentiometer_inclusive.ino
@@ -1,9 +1,9 @@
-
 // constants won't change. Used here to set a pin number :
-const int buttonPin = 10;     // the number of the pushbutton pin
+const int buttonPin = 3;     // the number of the pushbutton pin
 const int ledPin3 =  13;      // the number of the LED pin
 const int ledPin2 =  12;      // the number of the LED pin
 const int ledPin1 =  11;      // the number of the LED pin
+const int potentPin = 0;
 
 // Variables will change :
 bool ledState1;             // ledState used to set the LED
@@ -12,8 +12,10 @@ bool ledState3;             // ledState used to set the LED
 int buttonState;            // Saves the button state (HIGH/LOW)
 int lastButtonState;        // Saves last button state (HIGH/LOW)
 int buttonSetting;          // Saves what setting the button is on for our diffferent modes.
-int lastButtonSetting;       // Save the last button setting.
+int lastButtonSetting;      // Saves the last button setting.
 int tick;                   // Saves what tick the button is on (to differentiate the different lights for each setting)
+int potentValue;            // Saves the current potentiometer value
+int lightInterval;
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
@@ -21,8 +23,9 @@ unsigned long previousMillisLight;        // will store last time LED was update
 unsigned long previousMillisButton;       // store last time the button was checked to see if it was being pressed
 
 // constants won't change :
-const unsigned long lightInterval = 300;           // interval at which to blink (milliseconds)
 const unsigned long buttonInterval = 10;          // interval at which to check to see if the button is pressed
+const int minBlinkFrequency = 100;
+const int maxBlinkFrequency = 1000;
 
 void setup() {
  // initialize the LED pin as an output:
@@ -30,8 +33,12 @@ void setup() {
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
   
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  // initialize the pushbutton pin as an input and attach an interrupt
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonUpdate, CHANGE);
+
+  // Initialize the potentiometerPin as an input
+  pinMode(potentPin, INPUT);
 
   // Set variables' startings states
   ledState1 = HIGH;             
@@ -44,25 +51,22 @@ void setup() {
   tick = 0;
   previousMillisButton = 0;
   previousMillisLight = 0;
+  potentValue = -1;
   Serial.begin(9600);
+  lightInterval = 1;
 }
 
 void loop() {
+  interrupts();
   // Get the current time in milliseconds.
   unsigned long currentMillis = millis();
 
-  //Check to see if there is a large enough gap since the last button update
-  if ((currentMillis - previousMillisButton) >= buttonInterval){
-    //Make sure there was a change in button state and that the current button state is high
-    if (buttonState == HIGH && lastButtonState != HIGH) {
-          lastButtonSetting = buttonSetting;
-          buttonSetting = (buttonSetting + 1) % 5;
-        } 
-        
-    //Upkeep of variables
-    previousMillisButton = currentMillis;    
-    lastButtonState = buttonState;
-  }
+  // Get the current potentiometer value
+  potentValue = analogRead(potentPin);
+
+  //Set the light interval at which it will blink.
+  lightInterval = 100 + potentValue;
+  
   // Read the button state
   buttonState = digitalRead(buttonPin);
 
@@ -142,8 +146,28 @@ void loop() {
     digitalWrite(ledPin2, ledState2);
     digitalWrite(ledPin3, ledState3);  
   }
-  //Delay so the Arduino doesn't run at max speed all the time.
-  delay(3);
   
+  //Delay so the Arduino doesn't run at max speed all the time.
+  delay(50);
+}
+
+
+void buttonUpdate() {
+  //Get current time
+  long unsigned currentMillis = millis();
+  
+  //Check to see if there is a large enough gap since the last button update
+  if ((currentMillis - previousMillisButton) >= buttonInterval){
+    //Make sure there was a change in button state and that the current button state is high
+    if (buttonState == HIGH && lastButtonState != HIGH) {
+          lastButtonSetting = buttonSetting;
+          buttonSetting = (buttonSetting + 1) % 5;
+      } 
+        
+    //Upkeep of variables
+    previousMillisButton = currentMillis;    
+    lastButtonState = buttonState;
+  }
+
 }
 
