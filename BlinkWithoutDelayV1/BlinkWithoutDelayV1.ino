263337a7d5a10b96c101a6f92b876fdb2a28f204
Nicholas Sherman <nsherman@olin.edu>
2017-09-05 09:47:40 -0400

    updated code to use debouncing.

--- BlinkWithoutDelayV1/BlinkWithoutDelayV1.ino
+++ BlinkWithoutDelayV1/BlinkWithoutDelayV1.ino
@@ -32,16 +32,20 @@ const int ledPin1 =  11;      // the number of the LED pin
 bool ledState1;             // ledState used to set the LED
 bool ledState2;             // ledState used to set the LED
 bool ledState3;             // ledState used to set the LED
-int buttonState;            // Saves the button state (HIHG/LOW)
+int buttonState;            // Saves the button state (HIGH/LOW)
+int lastButtonState;        // Saves last button state (HIGH/LOW)
 int buttonSetting;          // Saves what setting the button is on for our diffferent modes.
+int lastButtonSetting;       // Save the last button setting.
 int tick;                   // Saves what tick the button is on (to differentiate the different lights for each setting)
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
-unsigned long previousMillis;        // will store last time LED was updated
+unsigned long previousMillisLight;        // will store last time LED was updated
+unsigned long previousMillisButton;       // store last time the button was checked to see if it was being pressed
 
 // constants won't change :
-const unsigned long interval = 500;           // interval at which to blink (milliseconds)
+const unsigned long lightInterval = 300;           // interval at which to blink (milliseconds)
+const unsigned long buttonInterval = 10;          // interval at which to check to see if the button is pressed
 
 void setup() {
  // initialize the LED pin as an output:
@@ -57,9 +61,12 @@ void setup() {
   ledState2 = LOW;            
   ledState3 = HIGH;           
   buttonState = 0;
+  lastButtonState = 0;
   buttonSetting = 0;
+  lastButtonSetting = 4;
   tick = 0;
-  previousMillis = 0;
+  previousMillisButton = 0;
+  previousMillisLight = 0;
   Serial.begin(9600);
 }
 
@@ -67,19 +74,26 @@ void loop() {
   // Get the current time in milliseconds.
   unsigned long currentMillis = millis();
 
+  //Check to see if there is a large enough gap since the last button update
+  if ((currentMillis - previousMillisButton) >= buttonInterval){
+    //Make sure there was a change in button state and that the current button state is high
+    if (buttonState == HIGH && lastButtonState != HIGH) {
+          lastButtonSetting = buttonSetting;
+          buttonSetting = (buttonSetting + 1) % 5;
+        } 
+        
+    //Upkeep of variables
+    previousMillisButton = currentMillis;    
+    lastButtonState = buttonState;
+  }
   // Read the button state
   buttonState = digitalRead(buttonPin);
 
-  // If the button is down, change the setting (having it bounded between 0 and 4)
-  if (buttonState == HIGH) {
-      buttonSetting = (buttonSetting + 1) % 5;
-      delay(500);
-    }
-
   // If there is a large enough gap since the last LED update
-  if ((currentMillis - previousMillis) >= interval) {
+  if ((currentMillis - previousMillisLight) >= lightInterval) {
+    
     // save the last time you blinked the LED
-    previousMillis = currentMillis;
+    previousMillisLight = currentMillis;
     // Increment the tick you're on every loop
     tick += 1;
 
@@ -152,7 +166,7 @@ void loop() {
     digitalWrite(ledPin3, ledState3);  
   }
   //Delay so the Arduino doesn't run at max speed all the time.
-  delay(5);
+  delay(3);
   
 }
 
